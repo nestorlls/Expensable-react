28c805c1a93c950347f209011266136c626046bd
Paul <llanquepol@gmail.com>
2023-05-26 20:08:07 -0500

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css';
 
 export const Hero = ({ title, children, alignTitle }) => {
   return (
-    <section className='hero'>
+    <section className="hero">
       {alignTitle === 'right' && children}
       <h1>{title}</h1>
       {alignTitle === 'left' && children}
